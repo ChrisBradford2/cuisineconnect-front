19d9a4769f1ebb359274324d87d06e32b494a4c0
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-09-12 14:30:15 +0200

    Add issues template.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head';
+import OpenAI from "openai";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,7 @@ interface HomeProps {
   data: UserData;
 }
 
-export default function Home({ data }: HomeProps) {
+export default function Home({ data, completionData }: any) {
   return (
     <>
       <Head>
@@ -28,6 +29,9 @@ export default function Home({ data }: HomeProps) {
         <p className="text-2xl text-center">
           Vous êtes connecté en tant que {data.name}.
         </p>
+        <h2 className="text-2xl text-center">
+          Voici une blague : {completionData.joke}
+        </h2>
       </main>
     </>
   )
@@ -36,11 +40,15 @@ export default function Home({ data }: HomeProps) {
 // Fetch data from internal API
 export async function getServerSideProps() {
   const response = await fetch('http://localhost:3000/api/hello')
+  const completion = await fetch('http://localhost:3000/api/completion')
   const data = await response.json()
+  const completionData = await completion.json()
   console.log(data)
+  console.log(completionData)
   return {
     props: {
-      data
+      data,
+      completionData
     }
   }
 }
