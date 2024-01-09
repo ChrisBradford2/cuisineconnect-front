1ad1aa027c57fc69ec68cb7a029e2bfebf9cc39b
Erwan <erwan.legloannec@outlook.com>
2024-01-09 17:18:39 +0100

    add dialect module

--- services/model.ts
+++ services/model.ts
@@ -1,10 +1,13 @@
 import { Sequelize } from 'sequelize';
 import User from '@/services/model/User';
 import Recipe from '@/services/model/Recipe';
+import pg from 'pg';
 
 const databaseUrl =
   process.env.DATABASE_URL ?? 'postgres://user:password@localhost:5432/db';
-const connection = new Sequelize(databaseUrl);
+const connection = new Sequelize(databaseUrl, {
+  dialectModule: pg,
+});
 
 const db = {
   User: User(connection),
